043195f0a1a03ec65698db3d14fe48c2a0679319
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2022-12-03 13:03:49 +0600

    updates

---
+++ src/components/graphics/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import playerContext from '../../context/playerContext'
+
+function Header() {
+  //const { currentSong, songs } = useContext(playerContext)
+
+  return (
+    <header className="draggable">
+      <h3>Kooh Music</h3>
+    </header>
+  )
+}
+
+export default Header
