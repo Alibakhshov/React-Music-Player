2c65dd9ea216099de5a98e3be3ed34888af41ae0
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2022-12-03 22:59:36 +0600

    Updates

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AudioPlayer from './AudioPlayer';
-
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AudioPlayer />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
+reportWebVitals();
