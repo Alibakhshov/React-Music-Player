5e91d9db4984f480c0161f19a0919e20e4d31945
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2022-12-03 14:17:00 +0600

    Updates

--- src/context/songs.js
+++ src/context/songs.js
@@ -4,7 +4,7 @@ export const song_list = [
     artistName: 'Skidmore College Orchestra',
     albumTitle: "Mussorgsky's Pictures at an Exhibition",
     fileUrl:
-      'https://files.freemusicarchive.org/storage-freemusicarchive-org/music/MusOpen/Skidmore_College_Orchestra/Mussorgskys_Pictures_at_an_Exhibition/Skidmore_College_Orchestra_-_01_-_Promenade_Allegro_giusto_nel_modo_russico_senza_allegrezza_ma.mp3',
+      'https://www.dropbox.com/home?preview=Muqom+-+Track+13.mp3',
   },
   {
     title: 'I. Gnomus Vivo',
